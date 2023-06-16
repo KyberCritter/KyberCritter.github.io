57ace0348304f8294af7592357f8011677096a94
KyberCritter <scott.a.ratchford@gmail.com>
2023-06-15 22:09:27 -0500

    removed htmly

--- htmly/system/admin/views/logout.html.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('HTMLY')) die('HTMLy'); ?>
-<?php
-
-unset($_SESSION[config("site.url")]);
-
-header('location: login');
-
-?>
--- htmly/themes/blog/no-posts.html.php
+++
@@ -1,2 +0,0 @@
-<?php if (!defined('HTMLY')) die('HTMLy'); ?>
-<h1><?php echo i18n('No_posts_found');?>!</h1>
